254e35db0ed256b382ee5585458d1d182ed94287
Rasmus Emil Christensen <s164408@student.dtu.dk>
2017-05-07 21:37:13 +0200

    Added even more commands.

--- src/dtu/softeng/group16/ui/SystemUI.java
+++ src/dtu/softeng/group16/ui/SystemUI.java
@@ -19,7 +19,8 @@ public class SystemUI{
         main = new Main();
         Scanner consoleReader = new Scanner(System.in);
         String output;
-        
+    
+        System.out.println("Type \"help\" to begin...");
         while(consoleReader.hasNext()){
             output = executeCommand(consoleReader.nextLine());
             System.out.println(output);
@@ -70,7 +71,7 @@ public class SystemUI{
         return employee;
     }
     
-    public static Project getProject(String key) {
+    public static Project getProject(String key) { // Rasmus
         Project project = main.getProjects().get(key);
         if (project==null) {
             throw new IllegalArgumentException("No project with id: " + key);
@@ -78,7 +79,7 @@ public class SystemUI{
         return project;
     }
     
-    public static void addActivity(Project p, Activity activity){
+    public static void addActivity(Project p, Activity activity){ // Rasmus
     
         HashSet<Activity> activities = p.getActivities();
         
@@ -87,21 +88,45 @@ public class SystemUI{
         
     }
     
+    public static Activity getActivity(String projectKey, String activityName){ // Rasmus
+        for(Activity a:getProject(projectKey).getActivities()) {
+            if(a.getName().equals(activityName)) {
+                return a;
+            }
+        }
+        throw new IllegalArgumentException("No activity with name: " + activityName);
+    }
+    
     private static String executeCommand(String input){ // Rasmus
         String[] c = input.split(" ");
         try{
             
             // Finds the correct command.
-            if(c[0].equals("add") || c[0].equals("new")){
+            if(c[0].equals("help")) {
+                return  "An empty argument is given with \"_\". All arguments with a star \"*\" cannot be empty.\n"+
+                        "Commands:\n" + 
+                        "\tadd project <name> <weeks from current week*> <owner>\n"+ 
+                        "\tadd activity <project id*> <name*> <budgetedTime*> <startWeek*> <duration*>\n"+ 
+                        "\tedit project manager <id*> <uuid>\n"+ 
+                        "\tedit project name <id*> <name>\n"+ 
+                        "\tedit activity name <id*> <name*> <newName*>\n"+ 
+                        "\tedit activity startWeek <id*> <name*> <startWeek*>\n"+ 
+                        "\tedit activity budgetedTime <id*> <name*> <budgetedTime*>\n"+ 
+                        "\tedit activity duration <id*> <name*> <duration*>\n"+ 
+                        "\treport project <id*> <week*>\n"+ 
+                        "\treport activity <id*> <name*> <week*>";
+            }
+            
+            else if(c[0].equals("add") || c[0].equals("new")){
                 
-                if(c[1].equals("employee")){ // Command: add employee <uuid>
+                if(c[1].equals("employee") || c[1].equals("e")){ // Command: add employee <uuid>
                     checkSize(c, 3);
                     checkNotEmpty(c[2]);
                     main.getEmployees().put(c[2], new Employee(c[2]));
                     return "Added new employee with uuid: " + c[2];
                 }
                 
-                else if(c[1].equals("project")){ // Command: add project <name> <weeks from current week*> <owner>
+                else if(c[1].equals("project") || c[1].equals("p")){ // Command: add project <name> <weeks from current week*> <owner>
                     checkSize(c, 5);
                     c[2] = setEmpty(c[2]);
                     checkNotEmpty(c[3]);
@@ -114,11 +139,11 @@ public class SystemUI{
                     }
                     return "Added new project \"" + project.getId() + "\":" +
                            "\n\tname:\t\t" + project.getName() +
-                           "\n\tstart week:\t" + project.getStartWeek() +
+                           "\n\tstart week:\t" + Main.formatWeek(project.getStartWeek()) +
                            "\n\towner:\t\t" + c[4];
                 }
                 
-                else if(c[1].equals("activity")){ // Command: add activity <project id*> <name*> <budgetedTime*> <startWeek*> <duration*>
+                else if(c[1].equals("activity") || c[1].equals("a")){ // Command: add activity <project id*> <name*> <budgetedTime*> <startWeek*> <duration*>
                     checkSize(c, 7);
                     checkNotEmpty(c[2]);
                     checkNotEmpty(c[3]);
@@ -132,18 +157,95 @@ public class SystemUI{
                     addActivity(getProject(c[2]), activity);
                     return "Added new activity \"" + activity.getName() + "\" in project " + c[2] + ":" +
                            "\n\tbudgeted time:\t" + activity.getBudgetedTime() +
-                           "\n\tstart week:\t\t" + activity.getStartWeek() +
+                           "\n\tstart week:\t\t" + Main.formatWeek(activity.getStartWeek()) +
                            "\n\tduration:\t\t" + activity.getDuration();
                 }
             }
             
-            else if(c[0].equals("report")){
+            else if(c[0].equals("report") || c[0].equals("r")){
                 
-                if(c[1].equals("project")){ // Command: report project <id*> <week*>
+                if(c[1].equals("project") || c[1].equals("p")){ // Command: report project <id*> <week*>
+                    checkSize(c, 4);
                     checkNotEmpty(c[2]);
                     checkNotEmpty(c[3]);
                     checkInt(c[3]);
                     return getProject(c[2]).report(Main.currentWeek() + Integer.parseInt(c[3]));
+                }
+                
+                else if(c[1].equals("activity") || c[1].equals("a")){ // Command: report activity <id*> <name*> <week*>
+                    checkSize(c, 5);
+                    checkNotEmpty(c[2]);
+                    checkNotEmpty(c[3]);
+                    checkNotEmpty(c[4]);
+                    checkInt(c[4]);
+                    return getActivity(c[2],c[3]).report(Main.currentWeek() + Integer.parseInt(c[4]));
+                }
+                
+            }
+            
+            else if(c[0].equals("edit")){
+                
+                if(c[1].equals("project") || c[1].equals("p")){
+                    
+                    if(c[2].equals("manager")){ // Command: edit project manager <id*> <uuid>
+                        checkSize(c, 5);
+                        checkNotEmpty(c[3]);
+                        c[4] = setEmpty(c[4]);
+                        getProject(c[3]).setManager(getEmployee(c[4]));
+                        return "Manager chanced to " + c[4] + ".";
+                    }
+                    
+                    else if(c[2].equals("name")){ // Command: edit project name <id*> <name>
+                        checkSize(c, 5);
+                        checkNotEmpty(c[3]);
+                        c[4] = setEmpty(c[4]);
+                        getProject(c[3]).setName(c[4]);
+                        return "Name chanced to " + c[4] + ".";
+                    }
+                    
+                }
+                
+                else if(c[1].equals("activity") || c[1].equals("a")){
+                    
+                    if(c[2].equals("name")){ // Command: edit activity name <id*> <name*> <newName*>
+                        checkSize(c, 6);
+                        checkNotEmpty(c[3]);
+                        checkNotEmpty(c[4]);
+                        checkNotEmpty(c[5]);
+                        getActivity(c[3],c[4]).setName(c[5]);
+                        return "Name of activity \"" + c[4] + "\" chanced to \"" + c[5] + "\".";
+                    }
+                    
+                    else if(c[2].equals("startWeek")){ // Command: edit activity startWeek <id*> <name*> <startWeek*>
+                        checkSize(c, 6);
+                        checkNotEmpty(c[3]);
+                        checkNotEmpty(c[4]);
+                        checkNotEmpty(c[5]);
+                        checkInt(c[5]);
+                        getActivity(c[3],c[4]).setStartWeek(Integer.parseInt(c[5]));
+                        return "Start week of activity \"" + c[4] + "\" chanced to \"" + c[5] + "\".";
+                    }
+                    
+                    else if(c[2].equals("budgetedTime")){ // Command: edit activity budgetedTime <id*> <name*> <budgetedTime*>
+                        checkSize(c, 6);
+                        checkNotEmpty(c[3]);
+                        checkNotEmpty(c[4]);
+                        checkNotEmpty(c[5]);
+                        checkInt(c[5]);
+                        getActivity(c[3],c[4]).setBudgetedTime(Integer.parseInt(c[5]));
+                        return "Budgeted time of activity \"" + c[4] + "\" chanced to \"" + c[5] + "\".";
+                    }
+                    
+                    else if(c[2].equals("duration")){ // Command: edit activity duration <id*> <name*> <duration*>
+                        checkSize(c, 6);
+                        checkNotEmpty(c[3]);
+                        checkNotEmpty(c[4]);
+                        checkNotEmpty(c[5]);
+                        checkInt(c[5]);
+                        getActivity(c[3],c[4]).setDuration(Integer.parseInt(c[5]));
+                        return "Duration of activity \"" + c[4] + "\" chanced to \"" + c[5] + "\".";
+                    }
+                    
                 }
                 
             }
